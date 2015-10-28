ded1d802bfc2d0f29e60c17bab49531e6e925cfa
cmy5kor <cmy5kor@KOR1030493.KOR.APAC.BOSCH.COM>
2015-10-28 11:52:32 +0530

    Testing Git

--- GitProject/src/TestGit.java
+++ GitProject/src/TestGit.java
@@ -2,6 +2,7 @@
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome!!!!!!!!!!!!!");
+		System.out.println("Welcome");
+		System.out.println("Testing");
 	}
 }
